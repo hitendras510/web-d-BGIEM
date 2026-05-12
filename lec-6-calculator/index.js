a8615a214f0b8a083d2b881d8943ddbdb38b8d52
Hitendra Singh <hitendrasingh5489@gmail.com>
2026-05-12 12:50:59 +0530

    init

--- lec-6-calculator/index.js
+++ lec-6-calculator/index.js
@@ -1,16 +1,44 @@
-let inp= document.querySelector("input")
-let buttons= document.querySelectorAll(".btn")
-for(let btn of buttons){
-    btn.addEventListener("click",()=>{
-        let btnText = btn.innerText
-        console.log(btnText);
-    if(btnText=="AC"){
-        inp.value=""
-    }else if(btnText=='='){
-        inp.value= eval(inp.value)
-    }else{
-        inp.value+=btnText
-    }
-        
-    })
+let inp = document.querySelector("#input");
+let buttons = document.querySelectorAll(".btn");
+
+for (let btn of buttons) {
+
+    btn.addEventListener("click", () => {
+
+        let btnText = btn.innerText;
+
+        if (btnText === "AC") {
+
+            inp.value = "";
+
+        }
+
+        else if (btnText === "=") {
+
+            try {
+
+                let expression = inp.value
+                    .replace(/×/g, "*")
+                    .replace(/÷/g, "/")
+                    .replace(/−/g, "-");
+
+                inp.value = eval(expression);
+
+            } 
+            catch {
+
+                inp.value = "Error";
+
+            }
+
+        }
+
+        else {
+
+            inp.value += btnText;
+
+        }
+
+    });
+
 }
